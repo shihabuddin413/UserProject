528377c76d5d4ff5f832ac6b0c29b34c0d8e237c
shihabe uddin <65333445+shihabuddin413@users.noreply.github.com>
2022-08-07 23:50:34 +0600

    Update tokens.py

--- tokens.py
+++ tokens.py
@@ -1,2 +1,7 @@
 tlb = "5434320022:AAEQqh-wCnk6YgE9v9RfNk-2d5BxsGp7G3I"
 bnb = "5406852207:AAEETw_X7Ja0tEj-TFlJDXi8M2thvjDNbhE"
+
+
+# https://beyondnativebot.pythonanywhere.com/admin/
+# user : beyondnativebot
+# pass : HelloBnb101
